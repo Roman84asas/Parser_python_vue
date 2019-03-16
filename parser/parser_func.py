4fbbc9286f2cb667ca2df4f15a87aaf931c8df1a
RomanMereneanu <asasin84@mail.ru>
2019-03-16 23:36:24 +0200

    Write class fnd json encode

--- parser/parser_func.py
+++ parser/parser_func.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import json
 
 
 # Make class for work with web site
@@ -11,10 +12,15 @@ class Parse:
     def sabscr(self):
         response = requests.get(self.site, timeout=5)
         content = BeautifulSoup(response.content, "html.parser")
-        for tag_linck in content.findAll('a', attrs={"class": "text-bold"}):
-            open = tag_linck.text.encode('utf-8')
-            print(open)
-
+        data_befor = []
+        i = 1
+        for tag_get in content.findAll('ol', attrs={"class": "pinned-items-list"}):
+            for tag_linck in content.findAll('span', attrs={"class": "repo js-pinnable-item"}):
+                data = tag_linck.text.encode('utf-8')
+                data_str = str(data)[2:-1]
+                data_befor.append(data_str)
+        with open('data.json', 'w') as outfile:
+            json.dump(data_befor, outfile)
 
 url = "https://github.com/Roman84asas"
 Parse(url).sabscr()
