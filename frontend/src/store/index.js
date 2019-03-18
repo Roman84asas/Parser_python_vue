98399d608785974cf18f6364741b9b95724e04c0
RomanMereneanu <asasin84@mail.ru>
2019-03-18 22:29:15 +0200

    Storege`s change.

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,12 +1,16 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import Axios from 'axios';
+import todos from './modules/todo';
 
 Vue.use(Vuex);
-
 
 export const store = new Vuex.Store({
   state: {},
   getters: {},
   mutations: {},
   actions: {},
+  modules: {
+    todos,
+  },
 });
---
+++ frontend/src/store/modules/todo.js
@@ -0,0 +1,11 @@
+const state = {};
+const getters = {};
+const mutations = {};
+const actions = {};
+
+export default {
+  state,
+  getters,
+  mutations,
+  actions,
+};
