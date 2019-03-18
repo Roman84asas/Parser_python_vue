2f32fbba0642663a9d49ce157b4af7141ea81c2a
RomanMereneanu <asasin84@mail.ru>
2019-03-18 21:15:29 +0200

    Add frontend in project

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="logo" src="./assets/Logo.png">
+    <Home msg="Parsing site"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {store} from './store/index';
 
 Vue.config.productionTip = false
 
+
 new Vue({
+  el: '#app',
+  store,
+  //router,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
